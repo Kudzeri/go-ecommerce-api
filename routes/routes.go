fdfaac562d2e74eadd8ac18563f3e34871dba74f
Kudzeri <kudzeri@gmail.com>
2024-07-16 15:24:49 +0500

    add user routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/Kuderi/go-ecommerce-api/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incommingRoutes *gin.Engine) {
+	incommingRoutes.POST("/users/signup", controllers.SigUp())
+	incommingRoutes.POST("/users/login", controllers.Login())
+	incommingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incommingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incommingRoutes.GET("/users/search", controllers.Search())
+}
